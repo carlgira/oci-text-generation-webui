8d31b1e9cb43dcddd4d3da07b20489c2478599cd
carlgira <carlgira@gmail.com>
2023-05-02 21:10:38 +0200

    Fix text-generation-webui

--- start.sh
+++ start.sh
@@ -116,8 +116,11 @@ else
 fi
 
 if [[ "$first_launch" -eq 1 ]]; then
+    "${python_cmd}" -m pip install --upgrade pip
     "${pip_cmd}" install -r requirements.txt
+    "${pip_cmd}" install texttable toml
     "${pip_cmd}" install --extra-index-url https://download.pytorch.org/whl/cu113 torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1+cu113
+
 
     cd models
     git lfs install
